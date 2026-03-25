0d8c1d79cd1fbb139ae644f03d7cac366f9e7216
Ecronika <t.paul@unter-der-sonne.de>
2026-03-26 00:07:43 +0100

    fix(db): resolve circular dependency in batch_alter_table

--- ticketsystem/migrations/versions/71aed8933587_added_ooo_fields_to_worker.py
+++ ticketsystem/migrations/versions/71aed8933587_added_ooo_fields_to_worker.py
@@ -19,6 +19,8 @@ depends_on = None
 def upgrade():
     with op.batch_alter_table('worker', schema=None) as batch_op:
         batch_op.add_column(sa.Column('is_out_of_office', sa.Boolean(), nullable=True))
+        
+    with op.batch_alter_table('worker', schema=None) as batch_op:
         batch_op.add_column(sa.Column('delegate_to_id', sa.Integer(), nullable=True))
         batch_op.create_foreign_key('fk_worker_delegate', 'worker', ['delegate_to_id'], ['id'])
 
@@ -27,4 +29,6 @@ def downgrade():
     with op.batch_alter_table('worker', schema=None) as batch_op:
         batch_op.drop_constraint('fk_worker_delegate', type_='foreignkey')
         batch_op.drop_column('delegate_to_id')
+        
+    with op.batch_alter_table('worker', schema=None) as batch_op:
         batch_op.drop_column('is_out_of_office')
